9d8ee09ec4df3ba5511a1dbb743398b88111c729
Luka <vujnovacluka89@gmail.com>
2021-07-25 01:21:11 +0200

    UI made for weather app

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            BackgroundView(isNight: $isNight)
+            VStack {
+                CityTextView(cityName: "Kaštel Lukšić")
+                
+                CurrentWeather(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 30)
+                    .padding(.bottom, 50)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "sun.max.fill",
+                                   temperature:28)
+                   
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "wind",
+                                   temperature: 25)
+                    
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "sunrise.fill",
+                                   temperature: 25)
+                    
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "cloud.rain.fill",
+                                   temperature: 23)
+                    
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 27)
+                        
+                    }
+                    
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                }label: {
+                    WeatherButton(title: "Change Mode",
+                                  textColor: .blue,
+                                  backgroundColor: .white)
+                }
+                
+                Spacer()
+                
+            }
+        }
     }
 }
 
@@ -19,3 +66,74 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50, alignment: .center)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 30, weight: .bold, design: .default))
+                .foregroundColor(.white)
+            
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,
+                                                   isNight ? .white : Color("lightBlue")]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding(.top, 30)
+    }
+    
+}
+
+struct CurrentWeather: View {
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        Image(systemName: imageName)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180, alignment: .center)
+        
+        Text("\(temperature)°")
+            .font(.system(size: 70, weight: .medium))
+            .foregroundColor(.white)
+    }
+}
+
+
